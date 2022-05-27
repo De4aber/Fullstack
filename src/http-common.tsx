19e62c8542d1310d69e9e66fcc6aa176c16d488e
Frann0 <deengo113@gmail.com>
2022-05-27 08:03:02 +0200

    updated base url to deployet api

--- src/http-common.tsx
+++ src/http-common.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: `https://localhost:7010/`,
+    baseURL: `http://185.51.76.204:8091/`,
     headers: {
         "Content-type": "application/json"
     }
